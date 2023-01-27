7cd144a921f04ec0b2b3ec7942bf6e783f4e00a7
D3N2-A <anmolsharma999937@gmail.com>
2023-01-27 20:08:58 +0530

    Function

--- Intro/variables.js
+++ Intro/variables.js
@@ -1,2 +1,6 @@
 var greetings = "Hello Anmol";
+//number
+var userId = 334;
+//boolean
+var isLoggedIn = false;
 console.log(greetings);
--- Intro/variables.ts
+++ Intro/variables.ts
@@ -1,3 +1,9 @@
 let greetings: string = "Hello Anmol";
 
+//number
+let userId = 334;
+
+//boolean
+let isLoggedIn: boolean = false;
+
 console.log(greetings);
