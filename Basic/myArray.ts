7d45c0723d2dd409cfb84ce063c802d8ccd0ffa7
D3N2-A <anmolsharma999937@gmail.com>
2023-01-28 09:58:32 +0530

    Arrays

---
+++ Basic/myArray.ts
@@ -0,0 +1,15 @@
+const superHeroes: string[] = [];
+// const heroPower: number[] = [];
+const heroPower: Array<number> = [];
+
+type User = {
+  name: string;
+  isActive: boolean;
+};
+
+const allUsers: User[] = [];
+const MLModels: number[][] = [];
+
+superHeroes.push("spiderman");
+heroPower.push(2);
+allUsers.push({ name: "", isActive: true });
