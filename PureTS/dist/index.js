2c6f9f8577f8be2ab22c06f741de169ae321a4d1
D3N2-A <anmolsharma999937@gmail.com>
2023-01-28 23:50:27 +0530

    private classes continued

--- PureTS/dist/index.js
+++ PureTS/dist/index.js
@@ -1,2 +1,10 @@
 "use strict";
 console.log("typescript is here still");
+class User {
+    constructor(email, name, userID) {
+        this.userID = userID;
+        this.city = "Jaipur";
+    }
+}
+const Anmol = new User("afssA@.com", "afas", "fas-dfasdfa-fa");
+Anmol.city;
--- PureTS/src/index.ts
+++ PureTS/src/index.ts
@@ -1,1 +1,9 @@
 console.log("typescript is here still");
+
+class User {
+  readonly city: string = "Jaipur";
+  constructor(email: string, name: string, private userID: string) {}
+}
+
+const Anmol = new User("afssA@.com", "afas", "fas-dfasdfa-fa");
+Anmol.city;
