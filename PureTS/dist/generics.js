9c320e50b6e9325066c2b54aa2c423e199f0d3e9
D3N2-A <anmolsharma999937@gmail.com>
2023-01-29 12:39:17 +0530

    generics

---
+++ PureTS/dist/generics.js
@@ -0,0 +1,22 @@
+"use strict";
+const score = [];
+const names = [];
+//We can use union, but we don't know what it will be
+// We can check using typeof but still bad practice
+function identityOne(val) {
+    return val;
+}
+//We can use forbidden method
+function identityTwo(val) {
+    return val;
+}
+/*We can declare type after function name and then use .
+passed value type is logged
+*/
+function identityThree(val) {
+    return val;
+}
+function identityFour(val) {
+    return val;
+}
+// identityFour<Bottle>({});
---
+++ PureTS/src/generics.ts
@@ -0,0 +1,36 @@
+const score: Array<number> = [];
+const names: Array<string> = [];
+
+//We can use union, but we don't know what it will be
+// We can check using typeof but still bad practice
+function identityOne(val: boolean | number): boolean | number {
+  return val;
+}
+
+//We can use forbidden method
+
+function identityTwo(val: any): any {
+  return val;
+}
+
+/*We can declare type after function name and then use .
+passed value type is logged
+*/
+function identityThree<Type>(val: Type): Type {
+  return val;
+}
+
+function identityFour<T>(val: T): T {
+  return val;
+}
+
+/*
+We can use generics to pass interface as type
+*/
+
+interface Bottle {
+  brand: string;
+  type: number;
+}
+
+// identityFour<Bottle>({});
