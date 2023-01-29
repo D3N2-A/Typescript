a26379cbe6f0395339598aee4d4809634937a62f
D3N2-A <anmolsharma999937@gmail.com>
2023-01-29 10:18:08 +0530

    private classes finished

--- PureTS/dist/index.js
+++ PureTS/dist/index.js
@@ -1,10 +1,26 @@
 "use strict";
-console.log("typescript is here still");
 class User {
     constructor(email, name, userID) {
+        this.email = email;
+        this.name = name;
         this.userID = userID;
+        this._courseCount = 1;
         this.city = "Jaipur";
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    deleteToken() {
+        console.log("Token Deleted");
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNum;
     }
 }
 const Anmol = new User("afssA@.com", "afas", "fas-dfasdfa-fa");
-Anmol.city;
--- PureTS/src/index.ts
+++ PureTS/src/index.ts
@@ -1,9 +1,28 @@
-console.log("typescript is here still");
-
 class User {
-  readonly city: string = "Jaipur";
-  constructor(email: string, name: string, private userID: string) {}
+  private _courseCount = 1;
+
+  private readonly city: string = "Jaipur";
+  constructor(
+    public email: string,
+    public name: string,
+    private userID: string
+  ) {}
+
+  get getAppleEmail(): string {
+    return `apple${this.email}`;
+  }
+  get courseCount(): number {
+    return this._courseCount;
+  }
+  private deleteToken() {
+    console.log("Token Deleted");
+  }
+  set courseCount(courseNum) {
+    if (courseNum <= 1) {
+      throw new Error("Course count should be more than 1");
+    }
+    this._courseCount = courseNum;
+  }
 }
 
 const Anmol = new User("afssA@.com", "afas", "fas-dfasdfa-fa");
-Anmol.city;
