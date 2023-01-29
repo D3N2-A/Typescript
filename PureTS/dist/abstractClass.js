a0e3b283928f2417148a13a6ca6d5fda805ca872
D3N2-A <anmolsharma999937@gmail.com>
2023-01-29 11:18:30 +0530

    abstarct classes

---
+++ PureTS/dist/abstractClass.js
@@ -0,0 +1,25 @@
+"use strict";
+class TakePhoto {
+    constructor(cameraMode, filter) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+    }
+    getReelTime() {
+        //some complex calculation
+        return 8;
+    }
+}
+// const anmol = new TakePhoto("potrait", "grayscale");
+class Instagram extends TakePhoto {
+    constructor(cameraMode, filter, burst) {
+        super(cameraMode, filter);
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burst = burst;
+    }
+    getSepia() {
+        console.log("sepia");
+    }
+}
+const anmo = new Instagram("potrait", "grayscale", 3);
+anmo.getReelTime();
